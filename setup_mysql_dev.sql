f010a1b6054fc67a4b785018debe715706036da8
attahchisom1clear <attahchisom1@gmail.com>
2023-03-23 10:54:32 +0100

    crating a user with granted permission to a database'

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,5 @@
 -- setting up a server to work on
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
 GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
-
 GRANT SELECT ON performnce_schema . * TO 'hbnb_dev'@'localhost';
