169eafc772a1d8cb28cbcd7223df9df080c66ab8
attahchisom6 <attahchisom6@gmail.com>
2023-03-25 09:22:51 +0100

    lets test ir

--- work_dir/models/city.py
+++ work_dir/models/city.py
@@ -7,5 +7,8 @@ from sqlalchemy import Integer, String, Column, ForeignKey
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
+
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+
+    places = relationship('Place', backref='cities', cascade='delete, delete-orphan, all')
--- work_dir/models/place.py
+++ work_dir/models/place.py
@@ -6,14 +6,14 @@ from sqlalchemy import String, Integer, Float, Column, ForeignKey
 class Place(BaseModel):
     """ makes a table of all placesi (including city)  user reside """
     __tablename__ = "places"
+
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeighnKey('users.id', nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=False)
-    number_rooms = Column(Integer, default=0)
-    number_bathrooms = Column(integer, default=0)
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
