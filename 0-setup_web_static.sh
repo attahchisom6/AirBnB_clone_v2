11b1fc1757483e95680cdb7be0fc46b0d3537350
attahchisom6 <attahchisom6@gmail.com>
2023-04-06 09:40:08 +0100

    deploying contents of my server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,51 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+#a bashscript that set up my web servwes for the deployment of webstatic
 
-apt-get update
-apt-get install -y nginx
+#updating the our machine and installing nginx
+sudo apt-get update
+sudo apt-get install -y nginx
 
+#starting to create web folders to store our dats
+sudo service nginx start
+mkdir -p /data/
+mkdir -p /data/web_static
+mkdir -p /data/web_static/releases/
+mkdir -p /data/web_static/shared
 mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
+echo 'Mastermose is the best, The lord God of Moses and Joshua is with him
+	unquenchable
+	A more than conqueror, conquering along his path
+	inexorable and unshakeable
+	Blessed be the lord God of Moses
+	' > /data/web_static/releases/test/index.html
+
+#forefully creating a symbolic link every time this script is run
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+#recursively given permission to specified user and group
+sudo chown -R ubuntu:ubuntu /data/
 
 printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
+	listen 80 default_server;
+	listen [::]:80 default_server;
+	add_header X-Served-By $HOSTNAME;
+	root /var/www/html;
+	index index.html index.htm;
+
+	location /hbnb_static {
+		alias /data/web_static/current;
+		index index.html index.htm;
+	}
+
+	location /redirect_me {
+		return 303 https://www.youtube.com;
+	}
+
+	error_page 404 /404.html;
+	location /404 {
+		root /var/www/html;
+		internal;
+	}
 }" > /etc/nginx/sites-available/default
 
-service nginx restart
+sudo service nginx restart
