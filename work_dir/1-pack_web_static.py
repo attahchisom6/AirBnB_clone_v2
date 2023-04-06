803bf49c0f5bd898badb6c0236117c880e1ad401
attahchisom6 <attahchisom@gmail.com>
2023-04-06 08:53:54 +0100

    compressing files using python fabric

---
+++ work_dir/1-pack_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+A fabric script that generates a tgz archieve from all in the
+web_static directory
+"""
+from fabric.api import local
+from datetime import datetime
+from os import path
+
+
+def do_pack():
+    """pack desired files into a comprrssed form
+    """
+    path.exists("versions") is False:
+        local("mkdir versions")
+
+    try:
+        now = datetime.now()
+        formatt = "%Y%m%d%H%M%S"
+        archieve_path = "versions/web_static_{}.tgz"
+        .format(now.strftime(formatt))
+        local("tar -cfvz {} web_static".format(archieve_path))
+        return (archieve_path)
+    except Exception:
+        return (None)
