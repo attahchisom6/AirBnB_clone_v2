69dd0c14eab6d5e745f96b8a1a13205b1fb409df
attahchisom6 <attahchisom6@gmail.com>
2023-04-07 09:09:56 +0000

    deploying to my servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,31 +1,34 @@
 #!/usr/bin/python3
-"""Distributes an archive to your web servers, using the function do_deploy"""
-from fabric.contrib import files
-from fabric.api import env, put, run
-import os
+"""
+script to deploy decompresses files to my servers
+"""
+from os import path
+from fabric.api import run, env, put
 
-env.hosts = ['100.25.191.25', '54.209.27.50']
+env.hosts = ["54.144.221.216", "54.209.215.95"]
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
-    """Function for deploy"""
-    if not os.path.exists(archive_path):
+    """
+    a function to deploy data to our servers
+    """
+    if not path.exists("archive_path"):
         return False
 
-    data_path = '/data/web_static/releases/'
-    tmp = archive_path.split('.')[0]
-    name = tmp.split('/')[1]
-    dest = data_path + name
+    temp = archive_path.split(".")[0]
+    name = temp.split('/')[1]
+    newPath = "/data/web_static/releases/" + name
 
     try:
-        put(archive_path, '/tmp')
-        run('mkdir -p {}'.format(dest))
-        run('tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
-        run('rm -f /tmp/{}.tgz'.format(name))
-        run('mv {}/web_static/* {}/'.format(dest, dest))
-        run('rm -rf {}/web_static'.format(dest))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(dest))
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(newPath))
+        run("tar -xzf /tmp/{}.tgz -C {}".format(name, newPath))
+        run("rm -rf /tmp/{}.tgz".format(name))
+        run("mv {}/web_static/* {}".format(newPath, newPath))
+        run("rm -rf {}/web_static".format(newPath))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(newPath))
         return True
     except Exception:
         return False
