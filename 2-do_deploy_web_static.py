8daf2aa5d41dbf7cf6cf304abadd5118b959a4e6
attahchisom6 <attahchisom@gmail.com>
2023-04-09 21:21:31 +0100

    deploying to  the server

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,31 +1,40 @@
 #!/usr/bin/python3
-"""script to deploy decompressed files to my web servers
-"""
-from os import path
-from fabric.api import run, env, put
-from fabric.contrib import files
+""" Function that compress a folder """
+from datetime import datetime
+from fabric.api import *
+import shlex
+import os
+
 
 env.hosts = ['54.144.221.216', '54.209.215.95']
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
-    """deploy decompressed files"""
-    if not path.exists(archive_path):
+    """ Deploys """
+    if not os.path.exists(archive_path):
         return False
-
-    temp = archive_path.split(".")[0]
-    name = temp.split("/")[1]
-    newPath = "/data/web_static/releases/" + name
-
     try:
-        put(archive_path, "/tmp")
-        run("mkdir -p {}".format(newPath))
-        run("tar -xzf /tmp/{}.tgz -C {}".format(name, newPath))
-        run("rm -rf /tmp/{}.tgz".format(name))
-        run("mv {}/web_static/* {}/".format(newPath, newPath))
-        run("rm -rf {}/web_static".format(newPath))
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
+
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
+
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(newPath))
+        run("ln -s {} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
         return True
-    except Exception:
+    except:
         return False
