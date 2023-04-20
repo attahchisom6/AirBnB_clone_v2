87ae09a2ad459103514bbe6dc7eb3fde2c9e79d8
attahchisom6 <attahchisom6@gmail.com>
2023-04-20 12:48:10 +0100

    greeting to friends in different routes

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
+"""
+atarts a web application that listens on poet 5000 and host 0.0.0.0
+
+routes:
+    "/": displays 'Hello HBNB!'
+    "/hbnb": displays 'HBNB'
 """
 from flask import Flask
 
@@ -11,16 +12,18 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
+def greet_hbnb():
+    """greet objs urls spxified in the route
+    """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'."""
+    """returns the string 'hbnb' on this path
+    """
     return "HBNB"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
