10bda30c17bb33b8d0c6f6c22e58d8ccd84c0020
attahchisom6 <attahchisom6@gmail.com>
2023-04-20 14:47:17 +0100

    web framework, print text with a sefault string

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>.
-    /python/(<text>): Displays 'Python' followed by the value of <text>.
+"""
+Script to imitiate a web application
 """
 from flask import Flask
 
@@ -13,35 +8,34 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
+def greet_hbnb():
+    """
+    function to return a greeting text
+    """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'."""
+    """return the string "HBNB'"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """Displays 'C' followed by the value of <text>.
-    Replaces any underscores in <text> with slashes.
+def text_without_underscore(text):
+    """prinnt the first argument inserted in the command line
     """
-    text = text.replace("_", " ")
-    return "C {}".format(text)
+    txt = text.replace("_", " ")
+    return "C {}".format(txt)
 
 
-@app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def python(text="is cool"):
-    """Displays 'Python' followed by the value of <text>.
-    Replaces any underscores in <text> with slashes.
+def python_text_without_underscore(text="cool"):
+    """print text without underacore, 'cool' is its default text
     """
-    text = text.replace("_", " ")
-    return "Python {}".format(text)
+    txt = text.replace("_", " ")
+    return "Python {}".format(txt)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
