397f8c72ef6ccae41323b7025fb89728b74d9016
attahchisom6 <attahchisom6@gmail.com>
2023-04-20 14:20:52 +0100

    web frame work; print text without underacore

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>.
+"""
+Script to imitiate a web application
 """
 from flask import Flask
 
@@ -12,23 +8,26 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
+def greet_hbnb():
+    """
+    function to return a greeting text
+    """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'."""
+    """return the string "HBNB'"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """Displays 'C' followed by the value of <text>."""
-    text = text.replace("_", " ")
-    return "C {}".format(text)
+def text_without_underscore(text):
+    """prinnt the first argument inserted in the command line
+    """
+    txt = text.replace("_", " ")
+    return "C {}".format(txt)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
