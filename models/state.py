7d973bcb932b3a01d6e64a94c7e9f791735f8df6
attahchisom6 <attahchisom6@gmail.com>
2023-04-21 19:33:24 +0100

    model state

--- models/state.py
+++ models/state.py
@@ -11,27 +11,25 @@ import models
 
 class State(BaseModel, Base):
     """ State class """
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = "states"
+    __tablename__ = "states"
 
-        name = Column(String(128), nullable=False)
-        cities = relationship(
+    name = Column(String(128), nullable=False)
+    cities = relationship(
                 'City', backref="state", cascade="delete, delete-orphan, all")
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
-        @property
-        def cities(self):
-            """returns a list of city instances with state_id = state.id
-            """
-            c_list = []
-            s_list = []
+    @property
+    def cities(self):
+        """returns a list of city instances with state_id = state.id
+        """
+        c_list = []
+        s_list = []
 
-            obj_dict = models.storage.all()
-            for key in obj_dict:
-                classname = key.split(".")[0]
-                if classname == "City":
-                    s_list.append(obj_dict[key])
-            for elem in s_list:
-                if elem.state_id == self.id:
-                    c_list.append(elem)
-            return (c_list)
+        obj_dict = models.storage.all()
+        for key in obj_dict:
+            classname = key.split(".")[0]
+            if classname == "City":
+                s_list.append(obj_dict[key])
+        for elem in s_list:
+            if elem.state_id == self.id:
+                c_list.append(elem)
+        return (c_list)
