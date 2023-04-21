dfaaeb19961e8b7acb35dbe746e6fc09ed4203a2
attahchisom6 <attahchisom6@gmail.com>
2023-04-21 18:36:28 +0100

    commiting

--- models/state.py
+++ models/state.py
@@ -1,39 +1,35 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column
-from sqlalchemy.orm import relationship
-from os import getenv
+""" holds class State"""
 import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class """
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = "states"
-
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref="state", cascade="delete, delete-orphan, all")
+    """Representation of state """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128),
+                      nullable=False)
+        cities = relationship("City", cascade="all, delete",
+                              backref="states")
     else:
         name = ""
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, *kwargs)
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            """returns a list of city instances with state_id = state.id
-            """
-            c_list = []
-            s_list = []
-
-            obj_dict = models.storage.all()
-            for key in obj_dict:
-                classname = key.split(".")[0]
-                if classname == "City":
-                    s_list.append(obj_dict[key])
-            for elem in s_list:
-                if elem.state_id == self.id:
-                    c_list.append(elem)
-            return (c_list)
+            """fs getter attribute that returns City instances"""
+            values_city = models.storage.all("City").values()
+            list_city = []
+            for city in values_city:
+                if city.state_id == self.id:
+                    list_city.append(city)
+            return list_city
