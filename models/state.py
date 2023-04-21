dd445a33a9c94a934974ddfe08d191e863152e37
attahchisom6 <attahchisom6@gmail.com>
2023-04-21 16:08:25 +0100

    all elements in the state

--- models/state.py
+++ models/state.py
@@ -3,17 +3,19 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
+from os import getenv
 import models
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref="state", cascade="delete, delete-orphan, all")
-
     if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "states"
+
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref="state", cascade="delete, delete-orphan, all")
+
+    else if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
             """returns a list of city instances with state_id = state.id
@@ -25,8 +27,8 @@ class State(BaseModel, Base):
             for key in obj_dict:
                 classname = key.split(".")[0]
                 if classname == "City":
-                    s_list.append(key)
+                    s_list.append(obj_dict[key])
             for elem in s_list:
-                if elem.state_id == State.id:
+                if elem.state_id == self.id:
                     c_list.append(elem)
             return (c_list)
