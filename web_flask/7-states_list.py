4ed7523361f2bd407cec4e489be13437b6f6d7c2
attahchisom6 <attahchisom6@gmail.com>
2023-04-22 14:46:50 +0100

    debugging this

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
-"""
-script to build a web application
-"""
-from flask import Flask
+""" Script that runs an app with Flask framework """
+from flask import Flask, render_template
 from models import storage
 from models.state import State
+
 
 app = Flask(__name__)
 
 
-@app.route("/state_list", strict_slashes=False)
-def show_states(self):
-    """display a list of states we currently have at the moment
-    """
-    states = storage.all(States).values()
-    return render_template("7-states_list.html", st=states)
+@app.teardown_appcontext
+def teardown_session(exception):
+    """ Teardown """
+    storage.close()
 
+
+@app.route('/states_list', strict_slashes=False)
+def display_html():
+    """ Function called with /states_list route """
+    states = storage.all(State)
+    dict_to_html = {value.id: value.name for value in states.values()}
+    return render_template('7-states_list.html',
+                           Table="States",
+                           items=dict_to_html)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
