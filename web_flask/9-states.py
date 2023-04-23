3a32cfceb8ea6277eb0ab06bbd94fc4a06647a1f
attahchisom6 <attahchisom6@gmail.com>
2023-04-23 01:22:52 +0100

    web framework: printing states with id

---
+++ web_flask/9-states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+web framework creating a web application
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """
+    delete curent session once its done
+    """
+    storage.close()
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def show_states():
+    """displays list of all available states in the current session
+    """
+    states = storage.all(State).values()
+    return render_template("9-states.html", st=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def state_by_id_contains_city(id):
+    """This will list all states whoses id matches the given id
+    """
+    states = storage.all(State).values()
+    for state in states:
+        if state.id = id:
+            return render_template("9-states.html", st=state)
+        else:
+            return render_template("9-states.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
